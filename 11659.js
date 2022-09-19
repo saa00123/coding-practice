ebb74e255649b747dfb9b31e83ddb77011ae701c
Choejeonghun <saa00123@naver.com>
2022-09-19 18:22:46 +0900

    coding-practice

--- 11659.js
+++ 11659.js
@@ -1,6 +1,20 @@
 // 구간 합
 
-const input = require("fs").readFileSync("example.txt").toString().split("\n");
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-// 문제 이해가 잘 안됨
-// 나중에 풀 예정
+let arr = input[0].split(" ").map(Number);
+let A = input[1].split(" ").map(Number);
+let N = arr[0];
+let M = arr[1];
+let sum = new Array(arr.length + 1).fill(0);
+let output = [];
+
+for (let i = 1; i <= N; i++) {
+  sum[i] = sum[i - 1] + A[i - 1];
+}
+
+for (let i = 0; i < M; i++) {
+  let [start, end] = input[i + 2].split(" ").map(Number);
+  output.push(sum[end] - sum[start - 1]);
+}
+console.log(output.join("\n"));
