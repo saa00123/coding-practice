4daf5950ab945f6717e0ca82e00f2219da14a708
Choejeonghun <saa00123@naver.com>
2022-09-19 17:47:09 +0900

    coding-practice

--- 1940.js
+++ 1940.js
@@ -1,6 +1,22 @@
 // 주몽
 
-let input = require("fs").readFileSync("example.txt").toString().split("\n");
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-// 문제 이해 x
-// 나중에 풀 예정
+let arr = input[2].split(" ").map(Number);
+let A = arr.sort((a, b) => a - b);
+let count = 0;
+let i = 0,
+  j = A.length - 1;
+
+while (i < j) {
+  if (A[i] + A[j] > input[1]) {
+    j--;
+  } else if (A[i] + A[j] < input[1]) {
+    i++;
+  } else {
+    count++;
+    i++;
+    j--;
+  }
+}
+console.log(count);
