1cda8d7b70a2a80142c64788006b9c7caef34234
Choejeonghun <saa00123@naver.com>
2022-09-19 17:40:33 +0900

    ready for practice

--- 11003.js
+++ 11003.js
@@ -3,6 +3,7 @@
 let input = require("fs").readFileSync("example.txt").toString().split("\n");
 
 let arr = input[0].split(" ").map(Number);
-let arr2 = input[1].split(" ").map(Number);
+let A = input[1].split(" ").map(Number);
+let N = arr[0];
 let L = arr[1];
-let D = [];
+let dq = [];
