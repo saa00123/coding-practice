70180b6735f1a46e3f73fd8b90f83ac9e6ad4d43
Choejeonghun <saa00123@naver.com>
2022-09-19 17:16:30 +0900

    코드 길이 단축 및 변경했지만 7%에서  실패

--- 1253.js
+++ 1253.js
@@ -1,36 +1,53 @@
 // 좋다
 
-let input = require("fs").readFileSync("example.txt").toString().split("\n");
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
 let arr = input[1].split(" ").map(Number);
 let A = arr.sort((a, b) => a - b);
 let count = 0;
 
 for (let k = 0; k < A.length; k++) {
-  let sum = A[k];
-  let i = 0;
-  let j = k - 1;
+  let i = 0,
+    j = k - 1;
 
   while (i < j) {
-    if (A[i] + A[j] == sum) {
-      if (i != k && j != k) {
-        count++;
-        break;
-      } else if (i == k) {
-        i++;
-      } else if (j == k) {
-        j--;
-      } else if (A[i]) {
-      }
-    } else if (A[i] + A[j] < sum) {
+    if (A[i] + A[j] < A[k]) {
       i++;
+    } else if (A[i] + A[j] > A[k]) {
+      j++;
     } else {
-      j--;
+      count++;
+      break;
     }
   }
 }
 
 console.log(count);
+
+// for (let k = 0; k < A.length; k++) {
+//   let sum = A[k];
+//   let i = 0;
+//   let j = k - 1;
+
+//   while (i < j) {
+//     if (A[i] + A[j] == sum) {
+//       if (i != k && j != k) {
+//         count++;
+//         break;
+//       } else if (i == k) {
+//         i++;
+//       } else if (j == k) {
+//         j--;
+//       }
+//     } else if (A[i] + A[j] < sum) {
+//       i++;
+//     } else {
+//       j--;
+//     }
+//   }
+// }
+
+// console.log(count);
 
 // 결과는 잘 나오지만 제출시 틀림
 // 3%에서 실패
